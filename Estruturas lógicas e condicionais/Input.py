abfff0ea28db607a26c454f119f5817be00b630a
Bianca Francis <kadu_300@live.com>
2023-11-13 10:19:39 -0300

    Update

---
+++ Estruturas lógicas e condicionais/Input.py
@@ -0,0 +1,7 @@
+#Função INPUT() - Função para receber dados do user
+
+print("Qual é o seu nome ?: ")
+nome = input()
+print ("Olá, ",nome)
+
+idade = input("Qual é a sua idade ?: ")
---
+++ Estruturas lógicas e condicionais/Tipo Boolean.py
@@ -0,0 +1,25 @@
+"""
+---------------------------------------------------------------------------------------------
+Tipo booleano é um tipo de dado primitivo que possuem dois valores, sendo eles TRUE ou FALSO.
+---------------------------------------------------------------------------------------------
+
+------------------------------------
+*Tipo boolean: True e False*
+Operadores lógicos:
+and (e)
+or (ou)
+not (não)
+------------------------------------
+Tabela verdade utilizando o AND:
+True and True = True
+True and False = False
+False and True = False
+False and False = False
+------------------------------------
+Tabela verdade utilizando o OR:
+True or True = True
+True or False = True
+False or True = True
+False or False = False
+------------------------------------
+"""
